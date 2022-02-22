bee40fe249530575c037d528d11c3f2d5ac32bec
KariSoup <l.y.ricxrdo@gmail.com>
2022-02-22 14:50:02 +0800

    I commit my live to this project

---
+++ DA_13.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class NewSpider(scrapy.Spider):
+    name = "new_spider"
+    start_urls = ['https://brickset.com/sets/year-2022']
+
+    def parse(self, response):
+        #xpath_selector = '//img'
+        css_selector='img'
+        #for x in response.xpath(xpath_selector):
+        for x in response.css(css_selector):
+            newsel = '@src'
+            yield {
+                'Image Link': x.xpath(newsel).extract_first(),
+            }
