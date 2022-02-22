0b60021c30bb7b0e0b8a6dd728debaff90613d8d
KariSoup <l.y.ricxrdo@gmail.com>
2022-02-22 15:04:59 +0800

    2json

--- DA_13.py
+++ DA_13.py
@@ -14,3 +14,13 @@ class NewSpider(scrapy.Spider):
             yield {
                 'Image Link': x.xpath(newsel).extract_first(),
             }
+
+# To recurse next page
+        Page_selector = '.next a ::attr(href)'
+        next_page = response.css(Page_selector).extract_first()
+        if next_page:
+            yield scrapy.Request(
+                response.urljoin(next_page),
+                callback=self.parse
+        )
+
